b88b813d00521fa4b5868d05ec3df23d56fee244
Akshat Agrawal <akshat160403@gmail.com>
2025-02-08 02:57:14 +0530

    fixed-bugs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 $('.owl-carousel').owlCarousel({
     loop:true,
+    autoplay: true,
     margin:10,
     nav:true,
     responsive:{
@@ -10,7 +11,7 @@ $('.owl-carousel').owlCarousel({
             items:3
         },
         1000:{
-            items:5
+            items:1
         }
     }
 })
