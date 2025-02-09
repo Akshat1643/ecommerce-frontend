099dabce9b0cf2ec8420f3bfe8c8ed6fd0f33f06
Akshat Agrawal <akshat160403@gmail.com>
2025-02-09 23:34:51 +0530

    fixed-bugs

--- owl.js
+++ owl.js
@@ -1,21 +1,23 @@
 $(document).ready(function () {
     $(".owl-carousel").owlCarousel({
-      loop: true,  // Infinite loop
-      margin: 0,  // Adjust space between images
-      nav: true,   // Navigation arrows
-      dots: false, // Hide navigation dots
+      loop: true,  
+      margin: 0, 
+      nav: true,   
+      dots: false, 
       autoplay: true,
-      autoplayTimeout: 1500,
+      autoplayTimeout:5000,
+      smartSpeed: 2000,        
+      autoplaySpeed: 2000, 
       autoplayHoverPause: true,
       responsive: {
         0: {
-          items: 1, // 1 item for mobile screens
+          items: 1, 
         },
         600: {
-          items: 3, // 3 items for medium screens
+          items: 3, 
         },
         1000: {
-          items: 9, // 5 items for larger screens
+          items: 9, 
         },
       },
     });
